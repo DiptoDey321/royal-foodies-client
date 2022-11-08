162927d9ae8f918288d28a80e931e66a5e836d63
Dipto Dey <diptodey2005@gmail.com>
2022-11-08 20:19:20 +0600

    login page added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
@@ -6,13 +7,13 @@ function Header() {
         <header className='mx-[10%] py-3'>
             <nav className="bg-[#270000] flex justify-between items-center">
                 <div className='logo'>
-                    <img className='w-[200px]' src="https://i.ibb.co/Nn12Kqt/r-foodies-logo.png" alt="" />
+                    <Link to='/'><img className='w-[200px]' src="https://i.ibb.co/Nn12Kqt/r-foodies-logo.png" alt="" /></Link>
                 </div>
                 <div className="">
                     <ul className='text-white flex items-center gap-x-5 text-xl Lato '>
                         <li>Add-Service</li>
                         <li>blog</li>
-                        <li>Login</li>
+                        <li><Link to='/login'>Login</Link></li>
                     </ul>
                 </div>
             </nav>
