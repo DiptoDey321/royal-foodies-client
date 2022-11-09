c4b290f8f29aa127621a236adf1e252d99fbe9bf
Dipto Dey <diptodey2005@gmail.com>
2022-11-09 20:26:56 +0600

    routing added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
+import ServiceDetails from './Components/Service-Details/ServiceDetails';
+import Services from './Components/Services/Services';
 import Layout from './Layout/Layout';
 
 function App() {
@@ -14,6 +16,15 @@ function App() {
         {
           path:'/',
           element : <Home></Home>
+        },
+        {
+          path:'/services',
+          loader: ()=> fetch('http://localhost:5000/allServices'),
+          element : <Services></Services>
+        },
+        {
+          path:'/service-details/:id',
+          element : <ServiceDetails></ServiceDetails>
         },
         {
           path:'/login',
